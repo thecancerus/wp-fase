401cd2d2974a8d9013ee6ec94bd0c627f7dfbf0b
Chris Mospaw <cmospaw@crowdfavorite.com>
2013-03-27 07:45:05 -0600

    parameter and doc block parsing in place. Some work on output

--- lib/wp_filter_action_parser.php
+++ lib/wp_filter_action_parser.php
@@ -18,10 +18,14 @@ class wp_filter_action_parser {
 		'do_action_ref_array',
 	);
 
+	private $processed_files; // Holder for files that were found, separated by file.
+	private $processed_finds; // Holder for actouns/filters that were found, separated by action/filter type.
+
 	function __construct($file_list = null, $verbose = true) {
 		$this->file_list = $file_list;
 		$this->verbose = $verbose;
 	}
+
 
 	function parse_file_list() {
 		if (is_array($this->file_list)) {
@@ -37,14 +41,12 @@ class wp_filter_action_parser {
 		// Check if file exists
 		if (!file_exists($file_name['fullpath'])) {
 			show("FILE NOT FOUND: " . $file_name['fullpath']);
-			
-			// **** Add error processing here ****
-			
+			// TODO: error processing here
 			return;
 		}
 
 		// Read contents of file
-		//show("PROCESSING: " . $file_name['fullpath']);
+		show("PROCESSING: " . $file_name['fullpath']);
 	
 		$file_contents = file_get_contents($file_name['fullpath']);
 		$file_lines = preg_split ('/$\R?^/m', $file_contents);
@@ -52,7 +54,6 @@ class wp_filter_action_parser {
 		// Split file contents into PHP tokens
 		$this->tokens = token_get_all($file_contents);
 
-//print_r($this->tokens);return;
 
 		// Iterate tokens
 		foreach ($this->tokens as $key => $token) {
@@ -61,21 +62,32 @@ class wp_filter_action_parser {
 
 					// TODO: check for function calls defining the string_to_parse
 					if ($this->is_function_call($key) == false) {
-						$parameters = $this->process_find($key);
-						show("\n" . $file_name['fullpath']);
-						show($token[1] . ' on line ' . $token[2] . " filter name: " . $parameters[0] );
+						$find = $this->process_raw_find($key);
+						$processor = 'processor_'.$token[1];
+
+						if (method_exists($this, $processor)) {
+							$this->$processor($token, $find, $file_name);
+						}
+						else {
+							echo "ERROR: PROCESSOR $processor DOES NOT EXIST.\n";
+						}
+
+						//show("\n" . $file_name['fullpath']);
+						//show($token[1] . ' on line ' . $token[2] . " filter name: " . $parameters[0] );
 					}
 				}
 			}
 			//$this->tokens[$key][0] = token_name($token[0]);
 		}
-		echo "-------------------------------------\n";
+		//show("-------------------------------------\n");
 		//print_r($this->tokens);
 	}
 
 
-	function process_find($key) {
-		echo "-------------------------------------\n";
+	function process_raw_find($key) {
+		//show("-------------------------------------\n");
+
+		$find_type = $this->tokens[$key][1];
 
 		// Search back for docblock
 		$docblock = $this->process_raw_docblock($key);
@@ -115,9 +127,10 @@ class wp_filter_action_parser {
 			}
 			$key++;
 		}
-		print_r($docblock . "\n");
-		print_r($parameters);
-		return $parameters;
+
+//		print_r($docblock . "\n");
+//		print_r($parameters);
+		return array('parameters' => $parameters, 'docblock' => $docblock);
 	}
 
 	function process_raw_parameters($key) {
@@ -194,5 +207,96 @@ class wp_filter_action_parser {
 		}
 
 	}
+
+	function clean_parameter($raw_parameter) {
+
+		if (strpos($raw_parameter, '=') !== false) {
+			list($parameter['name'], $parameter['default_value']) = explode('=',$raw_parameter, 2);
+		}
+		else {
+			$parameter['name'] = $raw_parameter;
+			$parameter['default_value'] = null;	
+		}
+
+		return $parameter;
+
+	}
+
+
+
+
+	// http://codex.wordpress.org/Function_Reference/apply_filters
+	function processor_apply_filters($token, $processor, $file_name) {
+		//var_dump($file_name);
+		//var_dump($token);
+		//var_dump($processor);
+
+		$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+
+		$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+
+		$tag = str_replace(array('"', "'"), '', array_shift($processor['parameters']));
+
+		$value = array_shift($processor['parameters']);
+		$vars = $processor['parameters'];
+
+		$message .= "apply_filter\n";
+		$message .= "  Hook: $tag\n";
+		$message .= "  Value to modify: $value\n";
+
+		if (count($vars) > 0) {
+			foreach ($vars as $var) {
+				$var = $this->clean_parameter($var);
+				$message .= "    - optional var: " . $var['name'];
+				if ($var['default_value'] !== null) {
+					$message .= ' | default value: ' . $var['default_value'];
+				}
+				$message .= "\n";
+			}
+		}
+
+
+		//echo $message;
+		return;
+	}
+
+	// http://codex.wordpress.org/Function_Reference/do_action
+	function processor_do_action($token, $processor, $file_name) {
+
+		$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+
+		$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+
+		$tag = str_replace(array('"', "'"), '', array_shift($processor['parameters']));
+
+		$value = array_shift($processor['parameters']);
+		$vars = $processor['parameters'];
+
+		$message .= "do_action\n";
+		$message .= "  Hook: $tag\n";
+
+		if (count($vars) > 0) {
+			foreach ($vars as $var) {
+				//$var = $this->clean_parameter($var);
+				$message .= "    - argument: " . $var;
+				$message .= "\n";
+			}
+		}
+
+		echo $message;
+		return;
+	}
+
+	// http://codex.wordpress.org/Function_Reference/do_action_ref_array
+	function processor_do_action_ref_array($token, $processor, $file_name) {
+		echo "DO ACTION REF ARRAY!\n";
+		return;
+	}
+
+
+
+
+
+
 
 }
