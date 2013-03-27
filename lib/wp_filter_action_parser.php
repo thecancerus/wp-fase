70027b287aa911efb1c0048dd992263be5728990
Chris Mospaw <cmospaw@crowdfavorite.com>
2013-03-27 16:14:15 -0600

    Better code organization and some cleanup. Add comments. Add some helper methods, remove others. Add gathering of data by file and hook type. Rudimentary HTML generation.

--- lib/wp_filter_action_parser.php
+++ lib/wp_filter_action_parser.php
@@ -4,7 +4,6 @@
  *		'dir' => $dir,
  *		'file' => $file,
  *		'fullpath' => $dir . '/' . $file,
- *
  */
 class wp_filter_action_parser {
 
@@ -21,23 +20,55 @@ class wp_filter_action_parser {
 	private $processed_files; // Holder for files that were found, separated by file.
 	private $processed_finds; // Holder for actouns/filters that were found, separated by action/filter type.
 
+	// Do a little setup
 	function __construct($file_list = null, $verbose = true) {
 		$this->file_list = $file_list;
 		$this->verbose = $verbose;
 	}
 
-
+	/**
+	 * Iterate files, parse them, trigger reports
+	 */
 	function parse_file_list() {
 		if (is_array($this->file_list)) {
 			foreach ($this->file_list as $file) {
 				$this->parse_file($file);
 			}
 		}
+		$this->assemble_reports();
 	}
 
+	/**
+	 * Pull out gathered information and output reports.
+	 */ 
+	function assemble_reports() {
+		echo "<html><head><title>EXAMPLE</title><body>";
+
+		ksort($this->processed_finds);
+		foreach ($this->processed_finds as $type => $finds) {
+			echo "<h1>$type</h1>";
+
+			ksort ($finds);
+			foreach ($finds as $find_name => $instances) {
+				echo "<h2>$find_name</h2>";
+				echo '<table border="1" cellspacing="0" cellpadding="3" width="100%">';
+				echo "<tr><td valign='top'>";
+				foreach ($instances as $instance) {
+					echo "<p>{$instance['file']['fullpath']} ({$instance['token'][2]})</p>";
+				}
+				echo "</td></tr>";
+				echo "</table><br>";
+			}
+		}
+
+		echo "</body></html>";
+
+	}
+
+	/**
+	 * Parse an indvidual file
+	 */
 	function parse_file($file_name) {
-
-
 		// Check if file exists
 		if (!file_exists($file_name['fullpath'])) {
 			show("FILE NOT FOUND: " . $file_name['fullpath']);
@@ -54,48 +85,39 @@ class wp_filter_action_parser {
 		// Split file contents into PHP tokens
 		$this->tokens = token_get_all($file_contents);
 
-
-		// Iterate tokens
+		// Iterate processed tokens
 		foreach ($this->tokens as $key => $token) {
 			if (is_array($token) && ($token[0] == T_STRING)) {
 				if (in_array($token[1], $this->strings_to_parse)) {
-
-					// TODO: check for function calls defining the string_to_parse
-					if ($this->is_function_call($key) == false) {
+					if ($this->is_function_definition($key) == false) {
 						$find = $this->process_raw_find($key);
 						$processor = 'processor_'.$token[1];
-
 						if (method_exists($this, $processor)) {
 							$this->$processor($token, $find, $file_name);
 						}
 						else {
 							echo "ERROR: PROCESSOR $processor DOES NOT EXIST.\n";
 						}
-
-						//show("\n" . $file_name['fullpath']);
-						//show($token[1] . ' on line ' . $token[2] . " filter name: " . $parameters[0] );
 					}
 				}
 			}
-			//$this->tokens[$key][0] = token_name($token[0]);
 		}
-		//show("-------------------------------------\n");
-		//print_r($this->tokens);
 	}
 
-
+	/**
+	 * If a match is found, process that match
+	 */
 	function process_raw_find($key) {
-		//show("-------------------------------------\n");
-
+		// What did we find?
 		$find_type = $this->tokens[$key][1];
 
 		// Search back for docblock
 		$docblock = $this->process_raw_docblock($key);
 
-		// Search forward for parameters
+		// Gather parameters
 		$parameters = array();
 
-		// gather array - find first "("
+		// find next "("
 		while ($key < count($this->tokens)) {
 			if ($this->tokens[$key] == '(') {
 				$key++;
@@ -104,8 +126,8 @@ class wp_filter_action_parser {
 			$key++;
 		}
 		$paren_count = 1;
+		// The closing parenthesis is the delimiter, but they might be nested...
 		while ($key < count($this->tokens)) {
-
 			if ($this->tokens[$key] == '(') {
 				$paren_count++;
 			}
@@ -116,23 +138,24 @@ class wp_filter_action_parser {
 			// Add information until ")" count is zero
 			if ($paren_count > 0) {
 				$processed = $this->process_raw_parameters($key);
+
 				// Add the parameter string
 				$parameters[] = $processed[0];
 				// Add the offset
 				$key += $processed[1];
 			}
-			// End of logical line...
+			// End of logical line or function call...
 			if ($this->tokens[$key] == ';' || $this->tokens[$key] == ')') {
 				break;
 			}
 			$key++;
 		}
-
-//		print_r($docblock . "\n");
-//		print_r($parameters);
 		return array('parameters' => $parameters, 'docblock' => $docblock);
 	}
 
+	/**
+	 * Find parameters by key, noting that parentheses can be nested
+	 */
 	function process_raw_parameters($key) {
 		$paren_count = 0;
 		$parameter = '';
@@ -147,7 +170,6 @@ class wp_filter_action_parser {
 			if ($this->tokens[$key] == ')') {
 				$paren_count--;
 			}
-
 			// Break on commas unless they're inside a paren
 			if (($this->tokens[$key] != ',' || $paren_count > 0) ) {
 				// Add anything that's a non-whitespace token
@@ -167,136 +189,175 @@ class wp_filter_action_parser {
 		return array($parameter, $offset);
 	}
 
+
+	/**
+	 * Find docblock info based on the key, return raw contents
+	 */
 	function process_raw_docblock ($key) {
 		$docblock = null;
-
 		while ($key > 0) {
 			// Detect closing curle braces or ; which indicate code that's not related
 			if ($this->tokens[$key] == ';' || $this->tokens[$key] == '}') {
 				break;
 			}
-
 			if (is_array($this->tokens[$key]) && $this->tokens[$key][0] == T_DOC_COMMENT) {
 				return $this->tokens[$key][1];
 			}
-
-
 			$key--;
 		}
-
-
 		return $docblock;
-
 	}
 
-	function is_function_call($key) {
 
-		// Ignore  whitespace
+	/**
+	 * See if the given key is a function definition
+	 */
+	function is_function_definition($key) {
 		while ($key > 0) {
+			// Ignore  whitespace
 			if (is_array($this->tokens[$key]) && ($this->tokens[$key][0] == T_WHITESPACE)) {
 				$key--;
 				continue;
 			}
+			// Is it T_FUNCTION? Yes, then we've got a function definition
 			if (is_array($this->tokens[$key]) && ($this->tokens[$key][0] == T_FUNCTION)) {
 				return true;
 			} 
+			// Anything else it's not a function definition
 			if (! is_array($this->tokens[$key])) {
 				return false;
 			}
 			$key--;
 		}
-
-	}
-
-	function clean_parameter($raw_parameter) {
-
-		if (strpos($raw_parameter, '=') !== false) {
-			list($parameter['name'], $parameter['default_value']) = explode('=',$raw_parameter, 2);
-		}
-		else {
-			$parameter['name'] = $raw_parameter;
-			$parameter['default_value'] = null;	
-		}
-
-		return $parameter;
-
+		return false;
 	}
 
 
+	/**
+	 * Convert quoted strings with no "$" in them to non-quoted strings
+	 * If there is a "$" present, convert all single quotes to double quotes for consistency
+	 */
+	function normalize_tag_names($tag) {
+
+		if (strpos($tag, '$') === false) {
+			$tag = str_replace(array('"', "'"), '', $tag);
+		} else {
+			$tag = str_replace(array("'"), '"', $tag);
+		}
+		return $tag;
+	}
 
 
 	// http://codex.wordpress.org/Function_Reference/apply_filters
-	function processor_apply_filters($token, $processor, $file_name) {
-		//var_dump($file_name);
-		//var_dump($token);
-		//var_dump($processor);
+	function processor_apply_filters($token, $find, $file_name) {
+		$tag= $this->normalize_tag_names(array_shift($find['parameters']));
+		$value = array_shift($find['parameters']);
+		$vars = $find['parameters'];
 
-		$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+		$this->processed_files[$file_name['fullpath']][$token[2]] = array(
+			'token' => $token,
+			'type' => 'apply_filters',
+			'hook' => $tag,
+			'optional_vars' => $vars,
+			'data' => $find,
+		);
 
-		$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+		$this->processed_finds['apply_filters'][$tag][] = array(
+			'token' => $token,
+			'file' => $file_name,
+			'hook' => $tag,
+			'optional_vars' => $vars,
+			'data' => $find,
+		);
 
-		$tag = str_replace(array('"', "'"), '', array_shift($processor['parameters']));
-
-		$value = array_shift($processor['parameters']);
-		$vars = $processor['parameters'];
-
-		$message .= "apply_filter\n";
-		$message .= "  Hook: $tag\n";
-		$message .= "  Value to modify: $value\n";
-
-		if (count($vars) > 0) {
-			foreach ($vars as $var) {
-				$var = $this->clean_parameter($var);
-				$message .= "    - optional var: " . $var['name'];
-				if ($var['default_value'] !== null) {
-					$message .= ' | default value: ' . $var['default_value'];
-				}
-				$message .= "\n";
-			}
-		}
-
-
+		//$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+		//$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+		//$message .= "apply_filter\n";
+		//$message .= "  Hook: $tag\n";
+		//$message .= "  Value to modify: $value\n";
+		//if (count($vars) > 0) {
+		//	foreach ($vars as $var) {
+		//		$message .= "    - optional var: " . $var;
+		//		$message .= "\n";
+		//	}
+		//}
 		//echo $message;
+
 		return;
 	}
 
 	// http://codex.wordpress.org/Function_Reference/do_action
-	function processor_do_action($token, $processor, $file_name) {
+	function processor_do_action($token, $find, $file_name) {
+		$tag = $this->normalize_tag_names(array_shift($find['parameters']));
+		$value = array_shift($find['parameters']);
+		$vars = $find['parameters'];
 
-		$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+		$this->processed_files[$file_name['fullpath']][$token[2]] = array(
+			'token' => $token,
+			'type' => 'do_action',
+			'hook' => $tag,
+			'optional_vars' => $vars,
+			'data' => $find,
+		);
 
-		$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+		$this->processed_finds['do_action'][$tag][] = array(
+			'token' => $token,
+			'file' => $file_name,
+			'hook' => $tag,
+			'optional_vars' => $vars,
+			'data' => $find,
+		);
 
-		$tag = str_replace(array('"', "'"), '', array_shift($processor['parameters']));
+		//$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+		//$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+		//$message .= "do_action\n";
+		//$message .= "  Hook: $tag\n";
+		//if (count($vars) > 0) {
+		//	foreach ($vars as $var) {
+		//		$message .= "    - argument: " . $var;
+		//		$message .= "\n";
+		//	}
+		//}
+		//echo $message;
 
-		$value = array_shift($processor['parameters']);
-		$vars = $processor['parameters'];
-
-		$message .= "do_action\n";
-		$message .= "  Hook: $tag\n";
-
-		if (count($vars) > 0) {
-			foreach ($vars as $var) {
-				//$var = $this->clean_parameter($var);
-				$message .= "    - argument: " . $var;
-				$message .= "\n";
-			}
-		}
-
-		echo $message;
 		return;
 	}
 
 	// http://codex.wordpress.org/Function_Reference/do_action_ref_array
-	function processor_do_action_ref_array($token, $processor, $file_name) {
-		echo "DO ACTION REF ARRAY!\n";
+	function processor_do_action_ref_array($token, $find, $file_name) {
+		$tag = $this->normalize_tag_names(array_shift($find['parameters']));
+		$arguments = $find['parameters'];
+
+		$this->processed_files[$file_name['fullpath']][$token[2]] = array(
+			'token' => $token,
+			'type' => 'do_action_ref_array',
+			'hook' => $tag,
+			'optional_vars' => $arguments,
+			'data' => $find,
+		);
+
+		$this->processed_finds['do_action_ref_array'][$tag][] = array(
+			'token' => $token,
+			'file' => $file_name,
+			'hook' => $tag,
+			'optional_vars' => $arguments,
+			'data' => $find,
+		);
+
+		//$message = "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n";
+		//$message .= $file_name['fullpath'] . ' on line ' . $token[2] . "\n";
+		//$message .= "do_action_ref_array\n";
+		//$message .= "  Hook: $tag\n";
+		//$message .= "  Argument array:\n";
+		//if (count($arguments) > 0) {
+		//	foreach ($arguments as $var) {
+		//		$message .= "    - array: " . $var;
+		//		$message .= "\n";
+		//	}
+		//}
+		//echo $message;
+
 		return;
 	}
-
-
-
-
-
-
 
 }
